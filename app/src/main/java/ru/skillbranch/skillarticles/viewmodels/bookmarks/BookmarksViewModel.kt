f3dd46a12c4f726f65214d978e093e090012e8c6
Dmitriy Panfilov <divasik@gmail.com>
2020-04-27 12:20:44 +0300

    class name fix

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/bookmarks/BookmarksViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/bookmarks/BookmarksViewModel.kt
@@ -3,5 +3,5 @@ package ru.skillbranch.skillarticles.viewmodels.bookmarks
 import androidx.lifecycle.ViewModel
 
 class BookmarksViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/transcriptions/TranscriptionsViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/transcriptions/TranscriptionsViewModel.kt
@@ -3,5 +3,5 @@ package ru.skillbranch.skillarticles.viewmodels.transcriptions
 import androidx.lifecycle.ViewModel
 
 class TranscriptionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
