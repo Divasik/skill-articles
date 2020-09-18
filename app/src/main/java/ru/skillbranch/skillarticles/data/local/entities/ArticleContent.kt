e4034b0213bec9f8b677b82c63547152bfeaaaa5
Dmitriy Panfilov <divasik@gmail.com>
2020-09-18 16:34:40 +0300

    dao link to test db fix

--- app/src/main/java/ru/skillbranch/skillarticles/data/local/entities/ArticleContent.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/entities/ArticleContent.kt
@@ -6,7 +6,13 @@ import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "article_contents")
+@Entity(tableName = "article_contents",
+foreignKeys = [ForeignKey(
+    entity = Article::class,
+    parentColumns = ["id"],
+    childColumns = ["article_id"],
+    onDelete = ForeignKey.CASCADE
+)])
 data class ArticleContent (
     @PrimaryKey
     @ColumnInfo(name = "article_id")
