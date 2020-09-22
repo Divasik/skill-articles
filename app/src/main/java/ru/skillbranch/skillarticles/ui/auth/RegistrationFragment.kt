b4a75d154eb7a272cd0c28ad5c672d60fab3a6e6
Dmitriy Panfilov <divasik@gmail.com>
2020-09-22 15:12:06 +0300

    constructor for test

--- app/src/main/java/ru/skillbranch/skillarticles/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/auth/RegistrationFragment.kt
@@ -1,17 +1,35 @@
 package ru.skillbranch.skillarticles.ui.auth
 
+import androidx.annotation.VisibleForTesting
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
+import androidx.savedstate.SavedStateRegistryOwner
 import kotlinx.android.synthetic.main.fragment_registration.*
 import ru.skillbranch.skillarticles.R
+import ru.skillbranch.skillarticles.ui.RootActivity
 import ru.skillbranch.skillarticles.ui.base.BaseFragment
 import ru.skillbranch.skillarticles.viewmodels.auth.AuthViewModel
 
-class RegistrationFragment : BaseFragment<AuthViewModel>() {
+class RegistrationFragment() : BaseFragment<AuthViewModel>() {
 
-    override val viewModel: AuthViewModel by viewModels()
+    var _mockFactory: ((SavedStateRegistryOwner) -> ViewModelProvider.Factory)? = null
+
+    override val viewModel: AuthViewModel by viewModels {
+        _mockFactory?.invoke(this) ?: defaultViewModelProviderFactory
+    }
+    
     override val layout: Int = R.layout.fragment_registration
     private val args: RegistrationFragmentArgs by navArgs()
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    constructor(
+            mockRoot: RootActivity,
+            mockFactory: ((SavedStateRegistryOwner)-> ViewModelProvider.Factory)? = null
+    ) : this() {
+        _mockRoot = mockRoot
+        _mockFactory = mockFactory
+    }
 
     override fun setupViews() {
         btn_register.setOnClickListener {
