042ba758beac284474f9ee6a57624fcacc546c03
Dmitriy Panfilov <divasik@gmail.com>
2021-04-14 22:54:48 +0300

    categories filter

---
+++ app/src/main/java/ru/skillbranch/sbdelivery/domain/filter/CategoriesFilterUseCase.kt
@@ -0,0 +1,23 @@
+package ru.skillbranch.sbdelivery.domain.filter
+
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.BiFunction
+import ru.skillbranch.sbdelivery.domain.entity.DishEntity
+import ru.skillbranch.sbdelivery.repository.DishesRepositoryContract
+import ru.skillbranch.sbdelivery.repository.error.EmptyDishesError
+
+class CategoriesFilterUseCase(private val repository: DishesRepositoryContract) : CategoriesFilter {
+
+    override fun categoryFilterDishes(categoryId: String): Single<List<DishEntity>> {
+        return repository.getCachedDishes()
+            .map { dishes ->
+                if(categoryId.isEmpty()) dishes
+                else dishes.filter { it.categoryId.equals(categoryId, true) }
+            }
+            .flatMap { dishes ->
+                if(dishes.isEmpty()) Single.error(EmptyDishesError("В этой категории нет блюд"))
+                else Single.just(dishes)
+            }
+    }
+
+}
--- app/src/main/java/ru/skillbranch/skillarticles/ui/article/IArticleView.kt
+++
@@ -1,9 +0,0 @@
-package ru.skillbranch.skillarticles.ui.article
-
-interface IArticleView {
-
-    fun showSearchBar()
-
-    fun hideSearchBar()
-
-}
